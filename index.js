d13179a3e2bb73a4e6542e53d75a37be60fa9d2b
Rohan Batra <rohanbatra@Rohans-MacBook-Pro.local>
2020-06-01 15:43:59 -0400

    refactor - move auth routes to new file

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 
 const cookieSession = require('cookie-session');
 
-const usersRepo = require('./repositories/users');
+const authRouter = require('./routes/admin/auth');
 
 const app = express();
 
@@ -16,78 +16,7 @@ app.use(
 	})
 );
 
-app.get('/signup', (req, res) => {
-	res.send(`
-    <div>   
-    Your id is: ${req.session.userId}
-        <form method='POST'>
-        <input name='email' placeholder='email'>
-        <input name='password' placeholder='password'>
-        <input name='passwordConfirmation' placeholder='password confirmation'>
-        <button>Sign Up</button>
-        </form>
-    </div>`);
-});
-
-app.post('/signup', async (req, res) => {
-	console.log(req.body);
-
-	const { email, password, passwordConfirmation } = req.body;
-
-	const existingUser = await usersRepo.getOneBy({ email });
-
-	if (existingUser) {
-		return res.send('Email in use');
-	}
-
-	if (password !== passwordConfirmation) {
-		return res.send('Passwords must match');
-	}
-
-	// create a user in the user repo
-	const user = await usersRepo.create({ email, password });
-
-	// store the id of that user inside the user's cookie
-	req.session.userId = user.id;
-
-	res.send('account created');
-});
-
-app.get('/signout', (req, res) => {
-	req.session = null;
-	res.send('You are logged out');
-});
-
-app.get('/signin', (req, res) => {
-	res.send(`
-    <div>   
-        <form method='POST'>
-        <input name='email' placeholder='email'>
-        <input name='password' placeholder='password'>
-        <button>Sign In</button>
-        </form>
-    </div>`);
-});
-
-app.post('/signin', async (req, res) => {
-	const { email, password } = req.body;
-
-	const user = await usersRepo.getOneBy({ email });
-
-	if (!user) {
-		return res.send('Email not found');
-	}
-
-	const validPassword = await usersRepo.comparePasswords(user.password, password);
-
-	if (!validPassword) {
-		return res.send('Invalid password');
-	}
-
-	req.session.userId = user.id;
-
-	res.send('You are signed in');
-});
+app.use(authRouter);
 
 app.listen(3000, () => {
 	console.log('listening on 3000');
---
+++ routes/admin/auth.js
@@ -0,0 +1,80 @@
+const express = require('express');
+
+const usersRepo = require('../../repositories/users');
+
+const router = express.Router();
+
+router.get('/signup', (req, res) => {
+	res.send(`
+    <div>   
+    Your id is: ${req.session.userId}
+        <form method='POST'>
+        <input name='email' placeholder='email'>
+        <input name='password' placeholder='password'>
+        <input name='passwordConfirmation' placeholder='password confirmation'>
+        <button>Sign Up</button>
+        </form>
+    </div>`);
+});
+
+router.post('/signup', async (req, res) => {
+	console.log(req.body);
+
+	const { email, password, passwordConfirmation } = req.body;
+
+	const existingUser = await usersRepo.getOneBy({ email });
+
+	if (existingUser) {
+		return res.send('Email in use');
+	}
+
+	if (password !== passwordConfirmation) {
+		return res.send('Passwords must match');
+	}
+
+	// create a user in the user repo
+	const user = await usersRepo.create({ email, password });
+
+	// store the id of that user inside the user's cookie
+	req.session.userId = user.id;
+
+	res.send('account created');
+});
+
+router.get('/signout', (req, res) => {
+	req.session = null;
+	res.send('You are logged out');
+});
+
+router.get('/signin', (req, res) => {
+	res.send(`
+    <div>   
+        <form method='POST'>
+        <input name='email' placeholder='email'>
+        <input name='password' placeholder='password'>
+        <button>Sign In</button>
+        </form>
+    </div>`);
+});
+
+router.post('/signin', async (req, res) => {
+	const { email, password } = req.body;
+
+	const user = await usersRepo.getOneBy({ email });
+
+	if (!user) {
+		return res.send('Email not found');
+	}
+
+	const validPassword = await usersRepo.comparePasswords(user.password, password);
+
+	if (!validPassword) {
+		return res.send('Invalid password');
+	}
+
+	req.session.userId = user.id;
+
+	res.send('You are signed in');
+});
+
+module.exports = router;
