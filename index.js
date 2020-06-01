aedd8d50a14a817033a270f12fef6e575bb37342
Rohan Batra <rohanbatra@Rohans-MacBook-Pro.local>
2020-06-01 12:57:33 -0400

    add signout and signin routes

--- index.js
+++ index.js
@@ -53,6 +53,40 @@ app.post('/signup', async (req, res) => {
 	res.send('account created');
 });
 
+app.get('/signout', (req, res) => {
+	req.session = null;
+	res.send('You are logged out');
+});
+
+app.get('/signin', (req, res) => {
+	res.send(`
+    <div>   
+        <form method='POST'>
+        <input name='email' placeholder='email'>
+        <input name='password' placeholder='password'>
+        <button>Sign In</button>
+        </form>
+    </div>`);
+});
+
+app.post('/signin', async (req, res) => {
+	const { email, password } = req.body;
+
+	const user = await usersRepo.getOneBy({ email });
+
+	if (!user) {
+		return res.send('Email not found');
+	}
+
+	if (user.password !== password) {
+		return res.send('Invalid password');
+	}
+
+	req.session.userId = user.id;
+
+	res.send('You are signed in');
+});
+
 app.listen(3000, () => {
 	console.log('listening on 3000');
 });
