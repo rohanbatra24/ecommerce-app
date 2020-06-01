4f74c24e8ad09ca49ada9b671166358c4e163ae3
Rohan Batra <rohanbatra@Rohans-MacBook-Pro.local>
2020-06-01 17:12:36 -0400

    refactor and add layout view

--- views/admin/auth/signup.js
+++ views/admin/auth/signup.js
@@ -1,11 +1,15 @@
+const layout = require('../layout');
+
 module.exports = ({ req }) => {
-	return `<div>   
-    Your id is: ${req.session.userId}
-        <form method='POST'>
-        <input name='email' placeholder='email'>
-        <input name='password' placeholder='password'>
-        <input name='passwordConfirmation' placeholder='password confirmation'>
-        <button>Sign Up</button>
-        </form>
-    </div>`;
+	return layout({
+		content : `<div>   
+        Your id is: ${req.session.userId}
+            <form method='POST'>
+            <input name='email' placeholder='email'>
+            <input name='password' placeholder='password'>
+            <input name='passwordConfirmation' placeholder='password confirmation'>
+            <button>Sign Up</button>
+            </form>
+        </div>`
+	});
 };
---
+++ views/admin/layout.js
@@ -0,0 +1,13 @@
+module.exports = ({ content }) => {
+	return `
+    <!DOCTYPE html>
+    <html>
+        <head>
+        </head>
+        <body>
+            ${content}
+        </body>
+    </html>
+    
+    `;
+};
