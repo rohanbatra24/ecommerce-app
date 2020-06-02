4643ea6b3499d304d5cd25610a1654b3f89d9854
Rohan Batra <rohanbatra@Rohans-MacBook-Pro.local>
2020-06-02 11:38:03 -0400

    import getError from helpers

--- views/admin/auth/signin.js
+++ views/admin/auth/signin.js
@@ -1,13 +1,18 @@
 const layout = require('../layout');
+const { getError } = require('../../helpers');
 
-module.exports = () => {
+module.exports = ({ errors }) => {
 	return layout({
-		content : `<div>   
-    <form method='POST'>
-    <input name='email' placeholder='email'>
-    <input name='password' placeholder='password'>
-    <button>Sign In</button>
-    </form>
-</div>`
+		content : `
+      <div>
+        <form method="POST">
+          <input name="email" placeholder="email" />
+          ${getError(errors, 'email')}
+          <input name="password" placeholder="password" />
+          ${getError(errors, 'password')}
+          <button>Sign In</button>
+        </form>
+      </div>
+    `
 	});
 };
--- views/admin/auth/signup.js
+++ views/admin/auth/signup.js
@@ -1,29 +1,21 @@
 const layout = require('../layout');
-
-const getError = (errors, prop) => {
-	// prop = email || password || passwordConfirmation
-	try {
-		return errors.mapped()[prop].msg;
-	} catch (err) {
-		return '';
-	}
-
-	//errors.mapped() = object with keys like email,password etc and ojb will have vmsg,
-};
+const { getError } = require('../../helpers');
 
 module.exports = ({ req, errors }) => {
 	return layout({
-		content : `<div>   
-        Your id is: ${req.session.userId}
-            <form method='POST'>
-            <input name='email' placeholder='email'>
-            ${getError(errors, 'email')}
-            <input name='password' placeholder='password'>
-            ${getError(errors, 'password')}
-            <input name='passwordConfirmation' placeholder='password confirmation'>
-            ${getError(errors, 'passwordConfirmation')}
-            <button>Sign Up</button>
-            </form>
-        </div>`
+		content : `
+    <div>
+      Your id is: ${req.session.userId}
+      <form method="POST">
+        <input name="email" placeholder="email" />
+        ${getError(errors, 'email')}
+        <input name="password" placeholder="password" />
+        ${getError(errors, 'password')}
+        <input name="passwordConfirmation" placeholder="password confirmation" />
+        ${getError(errors, 'passwordConfirmation')}
+        <button>Sign Up</button>
+      </form>
+    </div>
+  `
 	});
 };
