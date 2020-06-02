844a23d7e66c2a852a8054be14bc7ee319f18fb8
Rohan Batra <rohanbatra24@gmail.com>
2020-06-02 17:37:13 -0400

    reorder validation middleware for new products route

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -15,7 +15,7 @@ router.get('/admin/products/new', (req, res) => {
 	res.send(productsNewTemplate({}));
 });
 
-router.post('/admin/products/new', [ requireTitle, requirePrice ], upload.single('image'), async (req, res) => {
+router.post('/admin/products/new', upload.single('image'), [ requireTitle, requirePrice ], async (req, res) => {
 	const errors = validationResult(req);
 
 	const image = req.file.buffer.toString('base64');
