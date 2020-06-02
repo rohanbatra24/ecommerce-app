a716e2b39fd559dee5ec0ab9064fab9ffbff87fa
Rohan Batra <rohanbatra@Rohans-MacBook-Pro.local>
2020-06-01 23:27:48 -0400

    add validation messages for auth

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -15,7 +15,10 @@ router.get('/signup', (req, res) => {
 
 router.post('/signup', [ requireEmail, requirePassword, requirePasswordConfirmation ], async (req, res) => {
 	const errors = validationResult(req);
-	console.log(errors);
+
+	if (!errors.isEmpty()) {
+		return res.send(signupTemplate({ req, errors }));
+	}
 
 	const { email, password, passwordConfirmation } = req.body;
 	const user = await usersRepo.create({ email, password });
