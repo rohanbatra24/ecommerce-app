3d4e5df4416d0ec3d38212401272748d2823d9f3
Rohan Batra <rohanbatra24@gmail.com>
2020-06-02 12:56:53 -0400

    create products repo

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const productsRepo = require('../../repositories/products');
+
 const router = express.Router();
 
 router.get('/admin/products', (req, res) => {});
