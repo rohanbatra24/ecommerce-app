0e654199035100037cad6ffab15c6a6915303e8e
Rohan Batra <rohanbatra24@gmail.com>
2020-06-02 17:27:59 -0400

    update new product post route to get image string

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -15,10 +15,14 @@ router.get('/admin/products/new', (req, res) => {
 	res.send(productsNewTemplate({}));
 });
 
-router.post('/admin/products/new', [ requireTitle, requirePrice ], upload.single('image'), (req, res) => {
+router.post('/admin/products/new', [ requireTitle, requirePrice ], upload.single('image'), async (req, res) => {
 	const errors = validationResult(req);
 
-	console.log(req.file);
+	const image = req.file.buffer.toString('base64');
+
+	const { title, price } = req.body;
+
+	await productsRepo.create({ title, price, image });
 
 	res.send('submitted');
 });
