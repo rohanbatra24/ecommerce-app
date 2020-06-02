e67c673eefecfa3f9da9a51416c24b5b9b3c0d9a
Rohan Batra <rohanbatra24@gmail.com>
2020-06-02 14:51:10 -0400

    add post route for admin products new

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,8 +1,12 @@
 const express = require('express');
 
+const { validationResult } = require('express-validator');
+
 const productsRepo = require('../../repositories/products');
 
 const productsNewTemplate = require('../../views/admin/products/new');
+
+const { requireTitle, requirePrice } = require('./validators');
 
 const router = express.Router();
 
@@ -10,6 +14,12 @@ router.get('/admin/products', (req, res) => {});
 
 router.get('/admin/products/new', (req, res) => {
 	res.send(productsNewTemplate({}));
+});
+
+router.post('/admin/products/new', [ requireTitle, requirePrice ], (req, res) => {
+	const errors = validationResult(req);
+	console.log(errors);
+	res.send('submitted');
 });
 
 module.exports = router;
