e105d827ffabe3bdcf1e7667ba7fa7eda820d2bc
Rohan Batra <rohanbatra24@gmail.com>
2020-06-03 10:27:49 -0400

    import requireAuth and add to product routes as middleware

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const multer = require('multer');
 
-const { handleErrors } = require('./middlewares');
+const { handleErrors, requireAuth } = require('./middlewares');
 const productsRepo = require('../../repositories/products');
 const productsNewTemplate = require('../../views/admin/products/new');
 const productsIndexTemplate = require('../../views/admin/products/index');
@@ -10,26 +10,33 @@ const { requireTitle, requirePrice } = require('./validators');
 const router = express.Router();
 const upload = multer({ storage: multer.memoryStorage() });
 
-router.get('/admin/products', async (req, res) => {
+router.get('/admin/products', requireAuth, async (req, res) => {
+	if (!req.session.userId) {
+		return res.redirect('/signin');
+	}
 	const products = await productsRepo.getAll();
 	res.send(productsIndexTemplate({ products }));
 });
 
-router.get('/admin/products/new', (req, res) => {
+router.get('/admin/products/new', requireAuth, (req, res) => {
 	res.send(productsNewTemplate({}));
 });
 
 router.post(
 	'/admin/products/new',
+	requireAuth,
 	upload.single('image'),
 	[ requireTitle, requirePrice ],
 	handleErrors(productsNewTemplate),
 	async (req, res) => {
+		if (!req.session.userId) {
+			return res.redirect('/signin');
+		}
 		const image = req.file.buffer.toString('base64');
 		const { title, price } = req.body;
 		await productsRepo.create({ title, price, image });
 
-		res.send('submitted');
+		res.redirect('/admin/products');
 	}
 );
 
