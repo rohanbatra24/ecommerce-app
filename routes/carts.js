ed6fbf2335019402675298d808af1a919ec6f5bd
Rohan Batra <rohanbatra24@gmail.com>
2020-06-03 14:33:50 -0400

    update add to cart route

--- routes/carts.js
+++ routes/carts.js
@@ -1,30 +1,42 @@
 const express = require('express');
-
 const cartsRepo = require('../repositories/carts');
 
 const router = express.Router();
 
+// Receive a post request to add an item to a cart
 router.post('/cart/products', async (req, res) => {
-	//figure out the cart
-
+	// Figure out the cart!
 	let cart;
-
 	if (!req.session.cartId) {
-		// we dont have a cart, we need to create one
-		// store the cart id on the req.session.cartId
+		// We dont have a cart, we need to create one,
+		// and store the cart id on the req.session.cartId
 		// property
-
 		cart = await cartsRepo.create({ items: [] });
 		req.session.cartId = cart.id;
 	}
 	else {
-		// we have a cart, lets get it from the carts repo
+		// We have a cart! Lets get it from the repository
 		cart = await cartsRepo.getOne(req.session.cartId);
 	}
 
-	console.log(cart);
+	const existingItem = cart.items.find((item) => item.id === req.body.productId);
+	if (existingItem) {
+		// increment quantity and save cart
+		existingItem.quantity++;
+	}
+	else {
+		// add new product id to items array
+		cart.items.push({ id: req.body.productId, quantity: 1 });
+	}
+	await cartsRepo.update(cart.id, {
+		items : cart.items
+	});
 
-	res.send('product added to cart');
+	res.send('Product added to cart');
 });
+
+// Receive a GET request to show all items in cart
+
+// Receive a post request to delete an item from a cart
 
 module.exports = router;
